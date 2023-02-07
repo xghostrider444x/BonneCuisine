41841da8da8540968e6e41fff5532f00efe1687b
xghostrider444x <jescau10@gmail.com>
2023-02-07 10:21:19 -0500

    change in everything .

---
+++ sendEmail.php
@@ -0,0 +1,32 @@
+<?php 
+if (isset($_GET["action"])){
+    if($_GET["action"] == "envoyer"){
+        
+    }
+}
+$objet= "
+<p>Bonjour</p>
+<br><br>
+<p>Nous vous remercion d'avoir commandé via notre service de traiteur Bonne Cuisine.</p>
+<br>
+<p>Voici la description de votre commande</p>
+".afficherElementPanier()."
+
+
+
+
+
+
+"
+
+
+
+
+
+
+
+
+
+
+
+?>
