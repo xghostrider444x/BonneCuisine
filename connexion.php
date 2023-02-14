779e3f22573a93fcb65de4932cd0c81bcf7d85d0
xghostrider444x <jescau10@gmail.com>
2023-02-14 08:51:27 -0500

    Création du fichier connexion.php qui sert a ce connecter avec un usager.

---
+++ connexion.php
@@ -0,0 +1,12 @@
+<?php 
+include("include/head.inc.php");
+include("librairie/fonction.lib.php");
+$conn = new PDO('mysql:host=localhost; dbname=Bonne_Cuisine; charset=utf8','root','infoMac420');
+
+?>
+
+<div>
+    <form method="post" >
+
+    </form>
+</div>
