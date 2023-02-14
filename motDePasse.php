c54c34c2690535d68c98985b1538835782452953
xghostrider444x <jescau10@gmail.com>
2023-02-14 08:56:02 -0500

    Création de la page motDePasse.php qui sert a réinitialisé le mot de motDePasse

---
+++ motDePasse.php
@@ -0,0 +1,9 @@
+<?php 
+include("include/head.inc.php");
+include("librairie/fonction.lib.php");
+$conn = new PDO('mysql:host=localhost; dbname=Bonne_Cuisine; charset=utf8','root','infoMac420');
+
+
+
+
+?>
