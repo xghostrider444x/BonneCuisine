30e02866fc6326e6d0232011de66b0e02bcb1bd5
xghostrider444x <jescau10@gmail.com>
2023-02-21 08:58:57 -0500

    Création de la page ajouterMenu accessible uniquement en mode admin

---
+++ ajouterMenu.php
@@ -0,0 +1,40 @@
+<?php 
+include("include/headAdmin.inc.php");
+include("librairie/fonction.lib.php");
+$conn = new PDO('mysql:host=localhost; dbname=Bonne_Cuisine; charset=utf8','root','infoMac420');
+?>
+
+<div>
+    <form method="post" action="ajouterMenu.php?action=ajouter">
+    <div class="container pinkie">
+        <h3 class="text-center">Créer un nouveau menu</h3>
+        <div class="form-group row">
+        <label for="staticEmail" class="col-sm-2 col-form-label">Nom</label>
+        <div class="col-sm-10">
+        <input type="text" class="form-control" id="staticEmail" name="nom" placeholder="nom">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="staticEmail" class="col-sm-2 col-form-label">description</label>
+        <div class="col-sm-10">
+        <input type="text" class="form-control" id="description" name="description" placeholder="description">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">prix</label>
+        <div class="col-sm-10">
+        <input type="text" class="form-control" id="inputPassword" name="prix" placeholder="Prix">
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="text-center">   
+            <input type="submit" name="submit" value="Créer">      
+            <input type="reset" name="reset" value="Annuler">         
+    </div>
+</div>
+    </form>
+</div>
+<?php
+include("include/footAdmin.inc.php");
+?>
