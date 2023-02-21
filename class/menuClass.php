3533a0fa067f3eaaee8d97a3d52956b5b2b4c395
xghostrider444x <jescau10@gmail.com>
2023-02-21 09:12:21 -0500

    Codification de la class menuClass:     - Constructeur effectuer     - getter et setter effectués

--- class/menuClass.php
+++ class/menuClass.php
@@ -6,7 +6,46 @@ class Menu{
     private $prix;
 
     public function __construct(){
-        
+        $ctp = func_num_args();
+        $args = func_get_args();
+        if($ctp == 3){
+            setNom($args[0]);
+            setDescription($args[1]);
+            setPrix($args[2]);
+        }
     }
+
+    public function setIdMenu($idMenu){
+        $this->idMenu = $idMenu;
+    }
+    public function getIdMenu(){
+        return $this->idMenu;
+    }
+
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function setDescription($description){
+        $this->description = $description;
+    }
+    public function getDescription(){
+        return $this->description;
+    }
+
+    public function setPrix($prix){
+        $this->prix = $prix;
+    }
+    public function getPrix(){
+        return $this->prix;
+    }
+
+
+
+
+
 }
 ?>
