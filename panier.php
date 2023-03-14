65173c3af45e9afb9109fadced9661cbd8752770
xghostrider444x <jescau10@gmail.com>
2023-03-14 10:15:28 -0400

    Ajout de l'internationalisation

--- panier.php
+++ panier.php
@@ -4,6 +4,27 @@ session_start();
     include("librairie/fonction.lib.php");
     $conn = new PDO('mysql:host=localhost; dbname=Bonne_Cuisine; charset=utf8','root','infoMac420');
 
+    $lang = "fr";
+
+    if(isset($_GET["lang"])){
+        if($_GET["lang"] == "fr"){
+            $lang = "fr";
+        }
+        elseif($_GET["lang"] == "en"){
+            $lang = "en";
+        }
+    }
+
+    if(isset($_COOKIE['lang'])){
+        setcookie("lang", $lang, time()+365*24*60*60);
+    }
+    else{
+        setcookie("lang", $lang, time()+365*24*60*60);
+    }
+
+    $file_contents = file_get_contents('lang/'.$lang.'.json');
+
+    $data = json_decode($file_contents,true);
     if(isset($_COOKIE['panier'])){
         $panier = $_COOKIE["panier"]; 
         setcookie("panier", $panier, time()+3*60*60);
